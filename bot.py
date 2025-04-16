997ee0c3099e251ebc4295ef1be81ecaa1116625
Anzam2000 <andrejzamaraev29@gmail.com>
2025-04-16 21:08:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+import telebot
+from config import BOT_TOKEN, WEATHER_API_KEY
+from controllers import AirQualityController
 
+bot = telebot.TeleBot(BOT_TOKEN)
+controller = AirQualityController(bot, WEATHER_API_KEY)
+
+if __name__ == "__main__":
+    bot.polling()
